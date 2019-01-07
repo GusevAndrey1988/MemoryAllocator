e3f7fe7e5a32fdc2aca668cc82696c6bf58ee3f5
andrey <gusevandrey1988@gmail.com>
2019-01-08 02:04:36 +0300

    Вт янв  8 02:04:42 MSK 2019

--- PoolAllocator.cpp
+++ PoolAllocator.cpp
@@ -1,2 +1,47 @@
 #include "PoolAllocator.h"
 
+#include <cassert>
+
+PoolAllocator::PoolAllocator(size_t objectSize, u8 objectAlignment, size_t size, void *mem)
+	: IAllocator{size, mem}, _objectSize{objectSize}, _objectAlignment{objectAlignment}
+{
+	assert(objectSize >= sizeof(void*));
+
+	u8 adjustment = alignForwardAdjustement(mem, objectAlignment);
+	_free_list = static_cast<void**>(add(mem, adjustment));
+	size_t numObjects = (size - adjustment) / objectSize;
+	void **p = _free_list;
+
+	for (size_t i = 0; i < numObjects-1; ++i) {
+		*p = add(p, objectSize);
+		p = static_cast<void**>(*p);
+	}
+
+	*p = nullptr;
+}
+
+PoolAllocator::~PoolAllocator()
+{
+	_free_list = nullptr;
+}
+
+void* PoolAllocator::allocate(size_t size, u8 alignment)
+{
+	assert(size == _objectSize && alignment == _objectAlignment);
+
+	if (_free_list == nullptr) return nullptr;
+	void *p = _free_list;
+	_free_list = static_cast<void**>(*_free_list);
+	_used_memory += size;
+	++_num_allocations;
+
+	return p;
+}
+
+void PoolAllocator::deallocate(void *p)
+{
+	*((void**)p) = _free_list;
+	_free_list = (void**)p;
+	_used_memory -= _objectSize;
+	--_num_allocations;
+}
--- PoolAllocator.h
+++ PoolAllocator.h
@@ -1,9 +1,22 @@
 #ifndef _POOLALLOCATOR_H
 #define _POOLALLOCATOR_H
 
+#include "Allocator.h"
+
 class PoolAllocator : public IAllocator {
 private:
+	PoolAllocator(const PoolAllocator &)=delete;
+	PoolAllocator& operator=(const PoolAllocator &)=delete;
+
+	size_t _objectSize;
+	u8 _objectAlignment;
+	void **_free_list;
 public:
+	PoolAllocator(size_t objectSize, u8 objectAlignment, size_t size, void *mem);
+	~PoolAllocator();
+
+	void* allocate(size_t size, u8 alignment) override;
+	void deallocate(void *p) override;
 }; //PoolAllocator
 
 #endif //_POOLALLOCATOR_H
